7af4c589770192e114c6e82e10c080b894ed75c9
D <111471471+dgyt0214@users.noreply.github.com>
2023-07-21 12:33:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 $("#menu-toggle").click(function(e) {
-  e.preventDefault();
-  $("#sidebar-wrapper").width("250px");
-  $("#page-content-wrapper").css("padding-left", "250px");
-});
-
-$("#sidebar-wrapper").click(function(e) {
-  $(this).width("0");
-  $("#page-content-wrapper").css("padding-left", "100px");
-});
-
-$(document).mouseup(function(e) {
-  var sidebar = $("#sidebar-wrapper");
-
-  if (!sidebar.is(e.target) && sidebar.has(e.target).length === 0) {
-      sidebar.width("0");
-      $("#page-content-wrapper").css("padding-left", "100px");
-  }
-});
-
-
+    e.preventDefault();
+    $("#sidebar-wrapper").width("250px");
+    $("#page-content-wrapper").css("padding-left", "250px");
+  });
+  
+  $("#sidebar-wrapper").click(function(e) {
+    $(this).width("0");
+    $("#page-content-wrapper").css("padding-left", "100px");
+  });
+  
+  $(document).mouseup(function(e) {
+    var sidebar = $("#sidebar-wrapper");
+  
+    if (!sidebar.is(e.target) && sidebar.has(e.target).length === 0) {
+        sidebar.width("0");
+        $("#page-content-wrapper").css("padding-left", "100px");
+    }
+  });
+  
+  
+  
